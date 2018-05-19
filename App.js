4af7b13a6983f439cc2e951653db30b7f1c2336b
柳生昌宏 <gyu7372@gmail.com>
2018-05-19 19:26:19 +0900

    [add] t_32 reduxのaction,reducer周りの設定

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import devToolsEnhancer from 'remote-redux-devtools';
+import reducer from './reducers/';
 
 import Navigation from './src/Navigation';
 
-export default class App extends React.Component {
-  render() {
-    return (
-        <Navigation />
-    );
-  }
+const store = createStore(
+  reducer,
+  devToolsEnhancer(),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+
+function App() {
+  return (
+    <Provider store={store}>
+      <Navigation />
+    </Provider>
+  );
 }
+
+export default App;
---
+++ reducers/users.js
@@ -0,0 +1,23 @@
+import { handleActions } from 'redux-actions';
+
+import Actions from '../actions/';
+
+const users = handleActions(
+  {
+    [Actions.users.getuserinf](state, action) {
+      return Object.assign({}, state, action.payload);
+    },
+  },
+  {
+    id: 1,
+    like_person_twitter_id: 'aaa',
+    like_person_profile_image: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+    last_shoot_arrow_time: '2005-12-21 00:00:00.000',
+    profile_image: 'http://profile_image.jpg',
+    stock_arrow: 3,
+    comig_arrow: 10,
+    twitter_id: 'gyu73',
+  },
+);
+
+export default users;
