df634ef482badb187b3c4d1efb3053b32cd5101c
柳生昌宏 <gyu7372@gmail.com>
2018-05-22 07:50:32 +0900

    [add] t_42 矢を放つ確認ページにstore導入

--- src/screen/ConfirmShootArrowsScreen.js
+++ src/screen/ConfirmShootArrowsScreen.js
@@ -8,6 +8,11 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { Avatar, Button } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/Entypo';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { compose } from 'recompose';
+
+import Actions from '../../actions/';
 
   type Props = {
     navigation: {
@@ -49,10 +54,10 @@ function ConfirmShootArrowsScreen(props: Props) {
       <Avatar
         large
         rounded
-        source={{ uri: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg' }}
+        source={{ uri: props.users.like_person_profile_image }}
         activeOpacity={0.7}
       />
-      <Text style={{ paddingTop: 20, color: '#ffffff' }}>Ryota</Text>
+      <Text style={{ paddingTop: 20, color: '#ffffff' }}>{props.users.like_person_twitter_id}</Text>
       <Icon
         name="heart-outlined"
         size={50}
@@ -75,4 +80,21 @@ function ConfirmShootArrowsScreen(props: Props) {
   );
 }
 
-export default ConfirmShootArrowsScreen;
+const mapStateToProps = state => ({
+  users: state.users,
+});
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(
+    {
+      ...Actions.users,
+    },
+    dispatch,
+  );
+
+const Enhance = compose(connect(
+  mapStateToProps,
+  mapDispatchToProps,
+));
+
+export default Enhance(ConfirmShootArrowsScreen);
