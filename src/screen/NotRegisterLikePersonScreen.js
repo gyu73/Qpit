2a0fb034ce31b4eb901b8f0f41ae47e36e02499c
柳生昌宏 <gyu7372@gmail.com>
2018-06-19 14:52:22 +0900

    [add] master 好きな人登録ボタンの追加

--- src/screen/NotRegisterLikePersonScreen.js
+++ src/screen/NotRegisterLikePersonScreen.js
@@ -7,8 +7,13 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Button } from 'react-native-elements';
+import { connect } from 'react-redux';
+import { compose, withHandlers } from 'recompose';
+import { bindActionCreators } from 'redux';
 
-function NotRegisterLikePersonScreen() {
+import Actions from '../../actions/';
+
+function NotRegisterLikePersonScreen(props: Props) {
   const styles = StyleSheet.create({
     container: {
       height: '100%',
@@ -29,6 +34,19 @@ function NotRegisterLikePersonScreen() {
       shadowColor: 'black',
       shadowOpacity: 0.3,
     },
+    buttonStyles: {
+      width: 180,
+      height: 40,
+      borderColor: 'transparent',
+      borderWidth: 0,
+      borderRadius: 30,
+      backgroundColor: '#ffffff',
+      marginTop: 40,
+      marginBottom: 30,
+      shadowOffset: { width: 4, height: 4 },
+      shadowColor: 'black',
+      shadowOpacity: 0.3,
+    },
   });
   return (
     <View style={styles.container}>
@@ -36,11 +54,44 @@ function NotRegisterLikePersonScreen() {
         buttonStyle={styles.buttonStyle}
         color="#000000"
         fontWeight="900"
-        fontSize="14"
+        fontSize={14}
         title="好きな人を登録してないよ！好きな人を登録して矢を飛ばそう！"
+      />
+      <Button
+        onPress={() => props.handleRegisterLikePerson()}
+        title="好きな人を登録する"
+        buttonStyle={styles.buttonStyles}
+        color="#FF69B4"
+        fontWeight="900"
+        fontSize={14}
       />
     </View>
   );
 }
 
-export default NotRegisterLikePersonScreen;
+const mapStateToProps = state => ({
+  normal_hints: state.normal_hints,
+  users: state.users,
+});
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(
+    {
+      ...Actions.normalhints,
+    },
+    dispatch,
+  );
+
+const Enhance = compose(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps,
+  ),
+  withHandlers({
+    handleRegisterLikePerson: props => () => {
+      props.navigation.navigate('RegisterLikePerson');
+    },
+  }),
+);
+
+export default Enhance(NotRegisterLikePersonScreen);
